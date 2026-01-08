90bffc85194e51a682dd9b2c43e7745cb2e4fe0a
Tharun <tharun.wasantha@gmail.com>
2026-01-08 23:31:16 +0530

    fix some bugs

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { protect, adminOnly } = require("../middlewares/authMiddleware");
-const {  getDashboardData,  getUserDashboardData,  getTasks,  getTaskById,  createTask,  updateTask,  deleteTask,  updateTaskStatus,  updateTaskChecklist} = require("../controllers/taskController");
+const {  getDashboardData,  getUserDashboardData, getTaskById,  getTasks,  createTask,  updateTask,  deleteTask,  updateTaskStatus,  updateTaskChecklist} = require("../controllers/taskController");
 
 const router = express.Router();
 
@@ -8,16 +8,13 @@ const router = express.Router();
 // Dashboard Routes
 router.get("/dashboard-data", protect, getDashboardData);
 router.get("/user-dashboard-data", protect, getUserDashboardData);
-
 // Task Management Routes
-router.get("/", protect, getTasks); // Admin: all tasks, User: assigned tasks
-router.get("/:id", protect , getTaskById );
-
-router.post("/", protect, adminOnly, createTask);
-router.put("/:id", protect, updateTask); // consider adminOnly or ownership check
-router.delete("/:id", protect, adminOnly, deleteTask);
-
-router.put("/:id/status", protect, updateTaskStatus);
-router.put("/:id/todo", protect, updateTaskChecklist);
+router.get("/", protect, getTasks); // Get all tasks (Admin: all, User: assigned)
+router.get("/:id", protect , getTaskById ); // Get task by ID
+router.post("/", protect, adminOnly, createTask); // Create a new task (Admin only)
+router.put("/:id", protect, updateTask); // Update task details
+router.delete("/:id", protect, adminOnly, deleteTask); // Delete a task (Admin only)
+router.put("/:id/status", protect, updateTaskStatus); // Update task status
+router.put("/:id/todo", protect, updateTaskChecklist); // Update task Checklist
 
 module.exports = router;
