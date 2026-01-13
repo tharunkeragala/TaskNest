04f1543f7895d9656a87230eb598647fb9c80499
Tharun <tharun.wasantha@gmail.com>
2026-01-13 21:07:41 +0530

    Frontend - login page - right image

--- frontend/TaskNest/src/pages/Auth/Login.jsx
+++ frontend/TaskNest/src/pages/Auth/Login.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from "react";
+import AuthLayout from "../../components/layouts/AuthLayout";
 
 const Login = () => {
-  return (
-    <div>
-      Login
-    </div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
-export default Login
+  const navigare = useNavigate();
+  const handleLogin = async (e) => {
+    e.preventDefault();
+  };
+
+  return <AuthLayout>
+    <div className="">
+      <h3 className="">Welcome Back</h3>
+      <p className="">
+        Please enter your details to log in
+      </p>
+    </div>
+  </AuthLayout>;
+};
+
+export default Login;
