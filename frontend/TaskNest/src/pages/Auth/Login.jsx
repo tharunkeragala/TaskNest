f3ae9220df793bad4b8e26a936861864af0ba6b4
Tharun <tharun.wasantha@gmail.com>
2026-01-13 21:58:59 +0530

    fix frontend issue - login page

--- frontend/TaskNest/src/pages/Auth/Login.jsx
+++ frontend/TaskNest/src/pages/Auth/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import AuthLayout from "../../components/layouts/AuthLayout";
+import {useNavigate} from 'react-router-dom';
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -12,9 +13,9 @@ const Login = () => {
   };
 
   return <AuthLayout>
-    <div className="">
-      <h3 className="">Welcome Back</h3>
-      <p className="">
+    <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+      <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
+      <p className="text-xs text-slate-700 mt-[5px] mb-6">
         Please enter your details to log in
       </p>
     </div>
