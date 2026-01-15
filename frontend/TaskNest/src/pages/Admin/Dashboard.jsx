a2de8a01914c601984fc9a7633448e69045adfed
Tharun <tharun.wasantha@gmail.com>
2026-01-15 12:27:19 +0530

    error fixing in login

--- frontend/TaskNest/src/pages/Admin/Dashboard.jsx
+++ frontend/TaskNest/src/pages/Admin/Dashboard.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { useUserAuth } from "../../hooks/useUserAuth";
+import { UserContext } from "../../context/userContext";
 
 const Dashboard = () => {
-  return (
-    <div>
-      Dashboard
-    </div>
-  )
-}
+  useUserAuth();
 
-export default Dashboard
+  const { user } = useContext(UserContext);
+
+  return <div>Dashboard</div>;
+};
+
+export default Dashboard;
