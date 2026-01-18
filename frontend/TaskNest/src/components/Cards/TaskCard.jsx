12415960c24c84b7a497ff149c271cc3265c0de6
Tharun <tharun.wasantha@gmail.com>
2026-01-18 23:36:40 +0530

    Manage Task page initiate

---
+++ frontend/TaskNest/src/components/Cards/TaskCard.jsx
@@ -0,0 +1,110 @@
+import React from "react";
+import Progress from "../Progress";
+import AvatarGroup from "../AvatarGroup";
+import { LuPaperclip } from "react-icons/lu";
+import moment from "moment";
+
+const TaskCard = (
+  title,
+  description,
+  priotity,
+  status,
+  progress,
+  createdAt,
+  dueDate,
+  assignedTo,
+  attachmentCount,
+  completedTodoCount,
+  todoChecklist,
+  onClick,
+) => {
+  const getStatusTagColor = () => {
+    switch (status) {
+      case "In Progress":
+        return "text-cyan-500 bg-cyan-50 border-cyan-500/10";
+
+      case "Completed":
+        return "text-lime-500 bg-lime-50 border-lime-500/20";
+
+      default:
+        return "text-violet-500 bg-violet-50 border-violet-500/10";
+    }
+  };
+
+  const getPriorityTagColor = () => {
+    switch (priotity) {
+      case "Low":
+        return "text-emerald-500 bg-emerald-50 border border-emerald-500/10";
+      case "Low":
+        return "text-amber-500 bg-amber-50 border border-amber-500/10";
+      default:
+        return "text-rose-500 bg-rose-50 border border-rose-500/10";
+    }
+  };
+
+  return (
+    <div className="" onClick={onClick}>
+      <div className="">
+        <div
+          className={`text-[11px] font-medium ${getStatusTagColor()} px-4 py-0.5 rounded`}
+        >
+          {status}
+        </div>
+        <div
+          className={`text-[11px] font-medium ${getStatusTagColor()} px-4 py-0.5 rounded`}
+        >
+          {priotity} Priority
+        </div>
+      </div>
+      <div
+        className={`px-4 border-l-[3px] ${
+          status === "In Progress"
+            ? "border-cyan-500"
+            : status === "Completed"
+              ? "border-indigo-500"
+              : "border-violet-500"
+        }`}
+      >
+        <p className="">{title}</p>
+
+        <p className="">{description}</p>
+
+        <p className="">
+          Task Done:{" "}
+          <span className="">
+            {completedTodoCount} / {todoChecklist.length || 0}
+          </span>
+        </p>
+
+        <Progress progress={progress} status={status} />
+      </div>
+
+      <div className="">
+        <div className="">
+          <div>
+            <label className="">Start Date</label>
+            <p className="">{moment(createdAt).format("Do MMM YYYY")}</p>
+          </div>
+
+          <div>
+            <label className="">Due Date</label>
+            <p className="">{moment(dueDate).format("Do MMM YYYY")}</p>
+          </div>
+        </div>
+
+        <div className="">
+          <AvatarGroup avatars={assignedTo || []} />
+
+          {attachmentCount > 0 && (
+            <div className="">
+              <LuPaperclip className="" />{" "}
+              <span className="">{attachmentCount}</span>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TaskCard;
